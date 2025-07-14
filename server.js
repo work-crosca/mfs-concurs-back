b97a81c36e46e7977d55e9e6d76d7a53bf2928eb
FESK0 <work.crosca@gmail.com>
2025-07-14 20:45:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ import cors from "cors";
 import path from "path";
 import { connectDB } from "./db.js";
 import uploadRoutes from "./routes/upload.js";
+import likesRoutes from "./routes/likes.js";
+import imagesRoutes from "./routes/images.js";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(process.cwd(), "uploads")));
+app.use("/api/images", imagesRoutes);
+app.use("/api/likes", likesRoutes);
 
 await connectDB();
 
