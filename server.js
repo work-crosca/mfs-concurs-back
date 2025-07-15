f9b9c0b8d247d121c0811062dc13676a85fbbd9c
FESK0 <work.crosca@gmail.com>
2025-07-15 16:59:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import imagesRoutes from "./routes/images.js";
 
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(process.cwd(), "uploads")));
 app.use("/api/images", imagesRoutes);
